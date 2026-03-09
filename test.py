345a2b294a5e3b2712ef5c444c5f522a73acac88
Tammy Weng <tammy@hiremii.com>
2026-03-09 18:47:55 +0800

    Modify inner line

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 print ("Hello World")
 print("I'll add more context in the line")
-print("Let's play a game")
+print("Let's try to modify the internal lines an see if will be merge conflit.")
 print("test for the modify code and push again to the pull request.")
